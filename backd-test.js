e4d71d699612bd355104e3779528333f305e910a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-11 15:48:26 +0800

    Updated test.

--- backd-test.js
+++ backd-test.js
@@ -10,8 +10,8 @@ let error = new Error( "test error" );
 let result = "result";
 let option = { "hello": "world" };
 
-assert.deepEqual( callback( error, result, option ), [ error, result, option ], "should be deeply equal" );
+assert.deepEqual( callback( error, result, option ), [ error, result, option ], "should be equal to [ new Error( 'test error' ), 'result', { 'hello': 'world' } ]" );
 
-assert.deepEqual( callback( "yeah", "sample" ), [ error, result, option ], "should be deeply equal" );
+assert.deepEqual( callback( "yeah", "sample" ), [ error, result, option ], "should be equal to [ new Error( 'test error' ), 'result', { 'hello': 'world' } ]" );
 
 console.log( "ok" );
